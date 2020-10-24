497d8b98f4a27b0026bcd7a5e51e638d916fb4a2
Rud <alexander.rud84@gmail.com>
2020-10-24 12:53:34 +0300

    revision

--- btn1-2/js/main.js
+++ btn1-2/js/main.js
@@ -1,14 +1,20 @@
-let btn1 = document.querySelector('.btn1');
-let btn2 = document.querySelector('.btn2');
-let text = document.querySelector('.hello')
+// let btn1 = document.querySelector('.btn1');
+// let btn2 = document.querySelector('.btn2');
+// let text = document.querySelector('.hello')
 
-btn1.addEventListener('click', () =>{
-    btn2.classList.toggle('light')
-    text.classList.toggle('txt')
-})
-btn2.addEventListener('click', () =>{
-    text.classList.toggle('txt')
-})
+// btn1.addEventListener('click', () =>{
+//     btn2.classList.toggle('light')
+//     // text.classList.toggle('txt')
+    
+// })
+// btn2.addEventListener('click', () =>{
+//     text.classList.toggle('txt')
+// })
+const SPACES = [];
+const parking = [ 
+    place: {
+        
+    }
+]
 
-
-
+console.log(parking)
